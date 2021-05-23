a80e3cfe33bba88f4d4486baa4e6fb8625abc366
erhan kocabuga <erhankocabuga@gmail.com>
2021-05-23 18:16:34 +0300

    Bootstrap template has been applied

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import Header from "./components/Header";
 import AppSettings from "./general/AppSettings";
 
 function App() {
-    const [meal, setMeal] = useState(null);
+    const [ meal, setMeal ] = useState(null);
+    const [ isTopButtonVisible, setIsTopButtonVisible ] = useState(false);
+    const [ isGetButtonClicked, setIsGetButtonClicked ] = useState(false);
 
     async function getRandomRecipe() {
+      setIsGetButtonClicked(true);
       const randomRecipe = await axios(AppSettings.apis.randomRecipe);
       let meal = randomRecipe.data.meals[0];
       let ingredientList = [];
@@ -23,40 +26,84 @@ function App() {
 
       meal.ingredients = ingredientList;
       setMeal(meal);
-  }
+    };
 
-  const mealHtml = meal => {
-    return (
-        <div className="mealContainer">
-            <img src={meal.strMealThumb} style={{ width: 500 }} />  
-            <h2>Name: {meal.strMeal}</h2>
-            <p>Area: {meal.strArea}</p>
-            <p>Category: {meal.strCategory}</p>
-            <p>Tags: {meal.strTags}</p>
-            <p>Instructions: {meal.strInstructions}</p>
-            {meal.strTags && <p>Etiketler: {meal.strTags} </p>} 
+    const checkTopButtonVisibility = () => {
+      setIsTopButtonVisible(window.pageYOffset > 450);
+    };
 
-            { meal.strYoutube && (<div className="videoContainer">
-                <iframe
-                    width="560"
-                    height="315"
-                    src={`https://www.youtube.com/embed/${meal.strYoutube.split('=')[1]}`}
-                    title="YouTube video player"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen></iframe>
-            </div>) }
-        </div>
-    );
-  };
-     
-    return (
+    const goTopTheTop = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    };
+
+    const mealHtml = meal => {
+      return (
         <div>
-          <Header />
-          <p>Want a food idea? </p>
-          <button onClick={getRandomRecipe}>bring</button> 
-          { meal && mealHtml(meal) }
-        </div>
+          <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">{meal.strMeal}</h2> 
+            <div className="divider-custom">
+              <div className="divider-custom-line"></div>
+              <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+              <div className="divider-custom-line"></div>
+            </div>
+      
+            <div className="row justify-content-center">
+              <div className="col-md-12 col-lg-6 pb-2">
+                <img className="img-fluid" src={meal.strMealThumb} />
+              </div>
+              <div className="col-md-12 col-lg-6 pb-4">
+                <p className="card-text"><strong>Area:</strong> {meal.strArea}<br />
+                <strong>Category:</strong> {meal.strCategory}</p> 
+                <p className="card-text" dangerouslySetInnerHTML={{ __html: meal.strInstructions.replace(/\n/g, '<br />')}}></p>
+              </div>
+              <div className="col-12">
+                { meal.strTags &&
+                  <div className="card-footer">
+                    <small className="text-muted">Tags: {meal.strTags.replace(/,/g, ', ')}</small>
+                  </div> }
+              </div>
+            </div>
+        
+            <div className="row justify-content-center">
+                { meal.strYoutube && (<div className="video-container">
+                  <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Video of {meal.strMeal}</h2>
+                        <div className="divider-custom">
+                            <div className="divider-custom-line"></div>
+                            <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+                            <div className="divider-custom-line"></div>
+                        </div>  
+                        <div className="embed-container"><iframe src={`https://www.youtube.com/embed/${meal.strYoutube.split('=')[1]}`} frameBorder="0"></iframe></div>
+                        </div>)}
+              </div>
+          </div>
+      );
+    };
+
+    useEffect(() => {
+      window.addEventListener('scroll', checkTopButtonVisibility);
+    }, []);
+  
+    return (
+      <React.Fragment>
+        <Header onBringButtonClick={getRandomRecipe} isGetButtonClicked={isGetButtonClicked} />
+        <section className="page-section">
+            <div className="container">
+              { !isGetButtonClicked && <div className="text-center mt-4">
+                    <a className="btn btn-xl btn btn-primary" href="#" onClick={getRandomRecipe}>
+                        <i className="fas fa-cookie-bite me-2"></i>
+                        Bring
+                    </a>
+                </div> }
+
+              { meal && mealHtml(meal) }
+              </div> 
+        </section> 
+        { isTopButtonVisible && <div className="scroll-to-top" onClick={goTopTheTop}>
+          <a className="btn btn-outline-dark btn-social mx-1" href="#"><i className="fas fa-fw fa-arrow-up"></i></a>
+        </div>}
+      </React.Fragment>
     );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
-function Header() {
+function Header(props) {
     return (
-        <header>
-            <h1>What To Cook Today?</h1>
+        <header className="masthead bg-primary text-white text-center">
+            <div className="container d-flex align-items-center flex-column">
+            <h2 className="page-section-heading text-center text-uppercase text-white">Want an idea?</h2>
+            <div className="divider-custom divider-light">
+                <div className="divider-custom-line"></div>
+                <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+                <div className="divider-custom-line"></div>
+            </div>
+            <p className="masthead-subheading font-weight-light mb-0">You decided to cook at home and have no idea what to do? No problem, click the button and start get the suggestions.</p>
+            { props.isGetButtonClicked && <div className="text-center mt-4">
+                <a className="btn btn-xl btn-outline-light" href="#" onClick={props.onBringButtonClick}>
+                    <i className="fas fa-cookie-bite me-2"></i>
+                    Bring
+                </a>
+            </div> }
+            </div>
         </header>
     ); 
 };
